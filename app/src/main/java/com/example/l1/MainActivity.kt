bfbb7d99d413da2cd0ca7c143991794538595826
maoui810 <60634367+maoui810@users.noreply.github.com>
2021-08-05 16:42:03 -0700

    added clearinputs and hidekeyboard along with onclick listener

--- app/src/main/java/com/example/l1/MainActivity.kt
+++ app/src/main/java/com/example/l1/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.l1
 import android.content.ContentValues.TAG
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,18 +40,76 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG,"onCreate")
 
+        discountButton.setOnClickListener {
+            val firstName = firstName.text.toString().trim()
+            val lastName = lastName.text.toString().trim()
+            val email = email.text.toString()
+
+            if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()){
+                Toast.makeText(this, getString(R.string.add_text_validation), Toast.LENGTH_LONG).show()
+            }
+            else {
+                val fullName = firstName.plus(" ").plus(lastName)
+
+                discountCodeConfirmation.text =
+                    getString(R.string.discount_code_confirmation, fullName)
+                // Generates discount code
+                discountCode.text = UUID.randomUUID().toString().take(8).toUpperCase()
+
+                hideKeyboard()
+                clearInputFields()
+            }
+
+
+        }
+
 
     }
 
-    override fun onRestoreInstanceState(savedInstanceState:   Bundle) {
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         Log.d(TAG, "onRestoreInstanceState")
+
+        //Get the discount code or an empty string if it hasn't been set
+        discountCode.text = savedInstanceState.getString(DISCOUNT_CODE,"")
+        //Get the discount confirmation message or an empty string if it hasn't been set
+        discountCodeConfirmation.text =
+            savedInstanceState.getString(
+                DISCOUNT_CONFIRMATION_MESSAGE,"")
+
     }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         Log.d(TAG, "onSaveInstanceState")
+
+        outState.putString(DISCOUNT_CODE,
+            discountCode.text.toString())
+        outState.putString(DISCOUNT_CONFIRMATION_MESSAGE,
+            discountCodeConfirmation.text.toString())
     }
 
+
+
+    private fun clearInputFields() {
+        firstName.text.clear()
+        lastName.text.clear()
+        email.text.clear()
+    }
+
+    private fun hideKeyboard() {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val DISCOUNT_CONFIRMATION_MESSAGE = "DISCOUNT_CONFIRMATION_MESSAGE"
+        private const val DISCOUNT_CODE = "DISCOUNT_CODE"
+    }
 
 
 }
